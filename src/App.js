7323e0540c6bcac04136cc5e1a1966ceb1178e40
Christoffer Klevstad Buen <Christoffer.buen@gmail.com>
2019-12-05 17:05:45 +0100

    temp page
    temp page

--- src/App.js
+++ src/App.js
@@ -3,37 +3,23 @@ import Particles from 'react-particles-js';
 import './App.css';
 import Home from './Components/Home/Home'
 import About from './Components/About/About'
+import ThreeApp from './Components/ThreeApp/ThreeApp'
 import Projects from './Components/Projects/Projects'
 import particlesOptions from './particleOptions.js';
+import WebFont from 'webfontloader'; 
 
 // React-router import
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 
-const reactRoute = (paths)=>(
-  <Router>
-    <Switch>
-      {paths()}
-    </Switch>
-  </Router>
-);
-
-const route = ()=>(
-  <>
-  <Route path="/">
-    <Home />
-  </Route>
-  <Route path="/about">
-    <About />
-  </Route>
-  <Route path="/projects">
-    <Projects />
-  </Route>
-  </>
-);
+WebFont.load({
+  google: {
+    families: ['Titillium Web:300,400,700', 'sans-serif']
+  }
+});
 
 class App extends Component {
 
@@ -43,10 +29,22 @@ class App extends Component {
 
   render() {
     return (
-      <>
-      <Particles className="particles" params={particlesOptions}/>
-      {reactRoute(route)}
-      </>
+      <div className="app">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <div id="meDiv">
+              <h1>Christoffer Buen</h1>
+              <p>🎂26    👨‍Male    🇳🇴Norway</p>
+              <p>Informatics📚    Tech🖥    Space🛰</p>
+              <p id="meNote">Currently looking for work</p>
+              <p id="emailInfo">christoffer.buen[at]gmail.com</p>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+      <Particles className="particles" params={particlesOptions}></Particles>
+      </div>
     )}
 }
 
